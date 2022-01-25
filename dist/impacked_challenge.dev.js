36a30c3f9b417993b91ec068e7380270eb9067cc
fabiolamariapina <fabiolamariapina@gmail.com>
2022-01-25 10:50:31 -0500

    going to try a different method of downloading the json file because they way I am usually doing it is not working out

--- dist/impacked_challenge.dev.js
+++ dist/impacked_challenge.dev.js
@@ -1,11 +1,2 @@
-"use strict";
-
 // step 1: download json file
-var url = 'https://gist.githubusercontent.com/packardlabs/1c82043aef13c498f32ebc658f8ef297/raw/1ee1b8e35eba94195f6e485f653e09f8c03a9309/packtypes.json';
-fetch(url).then(function (res) {
-  return res.json();
-}).then(function (out) {
-  console.log('Checkout this JSON!', out);
-})["catch"](function (err) {
-  throw err;
-});
+"use strict";
--- impacked_challenge.js
+++ impacked_challenge.js
@@ -1,9 +1,1 @@
 // step 1: download json file
-let url = 'https://gist.githubusercontent.com/packardlabs/1c82043aef13c498f32ebc658f8ef297/raw/1ee1b8e35eba94195f6e485f653e09f8c03a9309/packtypes.json';
-
-fetch(url)
-.then(res => res.json())
-.then((out) => {
-    console.log('Checkout this JSON!', out)
-})
-.catch(err => { throw err })
