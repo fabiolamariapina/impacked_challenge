a3d0a2603274b078b3653ac4014ce98737f215df
fabiolamariapina <fabiolamariapina@gmail.com>
2022-01-25 11:18:11 -0500

    removed dist file since I don't need it

--- impacked_challenge.js
+++ impacked_challenge.js
@@ -2,7 +2,7 @@
 let url = 'https://gist.githubusercontent.com/packardlabs/1c82043aef13c498f32ebc658f8ef297/raw/1ee1b8e35eba94195f6e485f653e09f8c03a9309/packtypes.json';
 
 import fetch from "node-fetch"
-globalThis.fetch - fetch
+globalThis.fetch = fetch
 
 fetch(url)
     .then(res => res.json())
